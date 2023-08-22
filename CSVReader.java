c587040ae5ebb165dea085a6b591f490cb39ecc8
HanaeOukacha <133771958+HanaeOukacha@users.noreply.github.com>
2023-08-22 22:43:33 +0100

    CSVReader.java

--- CSVReader.java
+++ CSVReader.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CSVReader {
+//lire les données a partie d'un fichier csv
 	String filePath;
     public static List<User> readUsers(String filePath) throws IOException {
+// // Lire les données des utilisateurs à partir d'un fichier csv et creer une liste d'objets User
         List<User> users = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -21,12 +23,13 @@ public class CSVReader {
                 String idUser = parts[0];
                 String idElement = parts[1];
                 double note = Double.parseDouble(parts[2]);
-
+// Trouver ou créer l'utilisateur
                 User user = getUserById(users, idUser);
                 if (user == null) {
                     user = new User(idUser);
                     users.add(user);
                 }
+//ajout d'evaluation
                 user.addEvaluation(idElement, note);
             }
         }
@@ -35,11 +38,12 @@ public class CSVReader {
     }
 
     public static List<Element> readElements(String filePath) throws IOException {
+ // Lire les données des éléments à partir d'un fichier CSV et créer une liste d'objets Element
         List<Element> elements = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
-            boolean headerSkipped = false; // Skip the header line
+            boolean headerSkipped = false; //ignoer l'en tete
             while ((line = reader.readLine()) != null) {
                 if (!headerSkipped) {
                     headerSkipped = true;
@@ -49,19 +53,21 @@ public class CSVReader {
                 String idUser = parts[0];
                 String idElement = parts[1];
                 double note = Double.parseDouble(parts[2]);
-
+//trouver ou creer l'elemeng
                 Element element = getElementById(elements, idElement);
                 if (element == null) {
                     element = new Element(idElement);
                     elements.add(element);
                 }
+//ajouter l'evaluation
                 element.addEvaluation(idUser, note);
             }
         }
 
         return elements;
     }
-
+//trouver un objet user par son id
+//methode auxilliaire
     private static User getUserById(List<User> users, String idUser) {
         for (User user : users) {
             if (user.getIdUser().equals(idUser)) {
@@ -70,7 +76,8 @@ public class CSVReader {
         }
         return null;
     }
-
+//trouver l'element par id
+//methode auxilliaire
     private static Element getElementById(List<Element> elements, String idElement) {
         for (Element element : elements) {
             if (element.getIdElement().equals(idElement)) {
